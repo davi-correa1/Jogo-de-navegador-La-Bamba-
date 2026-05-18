ccbb91b38ebde7a1954c8e6f8196f0b095478fba
Davi Corrêa <davi.correa.ribeiro@gmail.com>
2026-05-18 18:54:15 -0300

    tem algum bug... mas ainda não identifiquei
    O servidor está funcionando bem demais. Com certeza tem algo errado!

--- server.js
+++ server.js
@@ -2,20 +2,44 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Estou definindo porta e hostname aqui
 const PORT = 5000;
 const HOSTNAME = 'localhost';
 
-app.use(express.static('public'));
+// Aqui estão as variaveis das pastas
+const pub = '/public';
+const view = '/views';
 
+// Aqui estão as variaveis das rotas
+const playEjs = pub + 'play.ejs';
+const indexHtml = pub + 'index.html';
+const createAccount = '';
+const chooseCharacter = '';
+
+// Rota principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, indexHtml));
 });
 
+// Rota para login 
 app.get('/Play', (req, res) => {
-    res.render('play.ejs')
+    res.render(playEjs)
 })
 
+// Rota para criar conta 
+app.get("/Create-Account", (req, res) => {
+    res.render(createAccount)
+})
+
+// Rota para escolher personagens
+app.get("/choose-character", (req, res) => {
+    res.render(chooseCharacter);
+})
+
+// Rotas do game...
+
+
 app.listen( PORT, HOSTNAME, () => {
-    console.clear
-    console.log(`http://${HOSTNAME}:${PORT}`);
+    //console.clear(); // Apenas limpa o terminal antes de mostrar a url
+    console.log(`http://${HOSTNAME}:${PORT}`); // Mostra a url
 });
