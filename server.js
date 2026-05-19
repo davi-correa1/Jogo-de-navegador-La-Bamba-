d33b69fcd9773fcd17226b60a9c1683720964e23
Davi Corrêa <davi.correa.ribeiro@gmail.com>
2026-05-19 08:59:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,39 +2,45 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('view', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Estou definindo porta e hostname aqui
 const PORT = 5000;
 const HOSTNAME = 'localhost';
 
-// Aqui estão as variaveis das pastas
-const pub = '/public';
-const view = '/views';
+// Aqui estão as variaveis das rotas HTML
+const HTML = {
+    index: '/public/index.html'
+};
 
-// Aqui estão as variaveis das rotas
-const playEjs = pub + 'play.ejs';
-const indexHtml = pub + 'index.html';
-const createAccount = '';
-const chooseCharacter = '';
+// Aqui estão as variaveis das rotas EJS 
+const EJS = {
+    play: '/views/play.ejs'
+    //createAccount:  '',
+    //chooseCharacter: ''
+};
 
 // Rota principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, indexHtml));
+    res.sendFile(path.join(__dirname, HTML.index));
 });
 
 // Rota para login 
-app.get('/Play', (req, res) => {
-    res.render(playEjs)
-})
+app.get('/play', (req, res) => {
+    res.render(EJS.play)
+});
 
 // Rota para criar conta 
 app.get("/Create-Account", (req, res) => {
-    res.render(createAccount)
-})
+    res.render(EJS.createAccount)
+});
 
 // Rota para escolher personagens
 app.get("/choose-character", (req, res) => {
-    res.render(chooseCharacter);
-})
+    res.render(EJS.chooseCharacter);
+});
 
 // Rotas do game...
 
