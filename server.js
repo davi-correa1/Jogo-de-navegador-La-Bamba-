9ab728ee2654c07f8004dde0df7db5404be73b7b
Davi Corrêa <davi.correa.ribeiro@gmail.com>
2026-05-20 14:50:01 -0300

    começando o banco de dados!

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const db = require('.db')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
